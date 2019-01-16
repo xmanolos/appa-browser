36aa76811efdb5ec59b25db8e4854b7160eecf05
Daniel Freitas <daniel.freitas@retta.com.br>
2019-01-15 23:37:17 -0200

    Reference to Swal and the facilitator in the base Blade. #6

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,11 +6,18 @@
 
         <title>DB Browser - @yield('title')</title>
 
-        <link rel="stylesheet" type="text/css" href="{{asset('css/patterns.css')}}">
-        <link rel="stylesheet" type="text/css" href="{{asset('css/master.css')}}">
-        <link rel="stylesheet" type="text/css" href="{{asset('css/components/text-fields.css')}}">
-        @yield('custom-styles')
+        <!-- Sweet Alert -->
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/sweetalert2@7.33.1/dist/sweetalert2.min.css">
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/sweetalert2@7.33.1/dist/sweetalert2.all.min.js"></script>
 
+        <!-- Custom -->
+        <script src="{{ asset('js/custom/swal.js') }}"></script>
+
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/patterns.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/master.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/components/text-fields.css') }}">
+        
+        @yield('custom-styles')
     </head>
     <body>
         @section('sidebar')
