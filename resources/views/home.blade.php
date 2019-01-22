4a3acd605c9fdab31a998d888a1bd28b6fc27d00
gabrielsecchi <gabrielsecchi@hotmail.com>
2019-01-22 18:55:11 -0200

    #2 Main page improvements

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,32 +2,58 @@
 
 @section('title', 'Connect')
 @section('custom-styles')
-    <link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}">
 @stop
 
 @section('content')
-
-<div class="center-center panel-connection">
-    <div class="row">
-        <div class="col-md-8 tfd panel-field" style="width: 450px;">
-            <input type="text" placeholder=" "/>
-            <span>Host</span>
+<div class="panel center-center panel-connection">
+    <form id="form">
+        <div class="row">
+            <div class="col tfd panel-field">
+                <select name="driver">
+                    <option value="">Select db type</option>
+                    <option value="mysql">MySQL</option>
+                    <option value="postgres">PostgreSQL</option>
+                </select>
+            </div>
+            <div class="col tfd panel-field">
+                <input name="database" type="text" placeholder=" "/>
+                <span>Database</span>
+            </div>
         </div>
-        <div class="col-md-4 tfd panel-field" style="width: 150px;">
-            <input type="text" placeholder=" "/>
-            <span>Port</span>
+        <div class="row">
+            <div class="col tfd panel-field" style="width: 70%;">
+                <input name="hostname" type="text" placeholder=" "/>
+                <span>Host</span>
+            </div>
+            <div class="col tfd panel-field" style="width: 30%;">
+                <input name="port" type="text" placeholder=" "/>
+                <span>Port</span>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-6 tfd panel-field" style="width: 300px;">
-            <input type="text" placeholder=" "/>
-            <span>Login</span>
+        <div class="row">
+            <div class="col tfd panel-field">
+                <input name="username" type="text" placeholder=" "/>
+                <span>Username</span>
+            </div>
+            <div class="col tfd panel-field">
+                <input name="password" type="password" placeholder=" "/>
+                <span>Password</span>
+            </div>
         </div>
-        <div class="col-md-6 tfd panel-field" style="width: 300px;">
-            <input type="password" placeholder=" "/>
-            <span>Password</span>
+        <div class="row right">
+            <div class="col">
+                <input id="btn-test" type="button" value="Test connection" class="button" />
+            </div>
+            <div class="col">
+                <input type="submit" value="Connect" class="button green" />
+            </div>
         </div>
-    </div>
+    </form>
 </div>
 
+@stop
+
+@section('custom-js')
+<script type="text/jscript" src="{{asset('js/test-connection.js')}}" ></script>
 @stop
