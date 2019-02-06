76e5c79a807371ca9f67ab9ac791c10f45fc7191
Daniel Freitas <daniel1208x@gmail.com>
2019-02-06 21:27:35 -0200

    New test data

--- public/js/custom/test.js
+++ public/js/custom/test.js
@@ -1,8 +1,8 @@
 function fillConnectionData() {
     $('select[name=driver]').val('mysql');
-    $('input[name=database]').val('dbbrowser2');
-    $('input[name=hostname]').val('db4free.net');
+    $('input[name=database]').val('sgq_test');
+    $('input[name=hostname]').val('sgq-auditoria.cfbxb7e74oyj.sa-east-1.rds.amazonaws.com');
     $('input[name=port]').val('3306');
-    $('input[name=username]').val('rootdb8');
-    $('input[name=password]').val('12345678');
+    $('input[name=username]').val('root');
+    $('input[name=password]').val('sam12345');
 }
