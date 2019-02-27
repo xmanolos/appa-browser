ceced356e165d303810b2a0a55ffbc2f05287c76
Daniel Freitas <daniel1208x@gmail.com>
2019-02-26 23:01:42 -0300

    Fixing CSS and JS author references.

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,16 +8,16 @@
 
         <title>DB Browser - @yield('title')</title>
 
-        <!-- Sweet Alert -->
+        <!-- SweetAlert2 (By SweetAlert2) -->
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/sweetalert2@7.33.1/dist/sweetalert2.min.css">
 
-        <!-- Font Awesome -->
+        <!-- Font Awesome (By FortAwesome) -->
         <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
-        <!-- icons8 line-awesom -->
+        <!-- Icons8 Line-Awesome (By Icons8) -->
         <link rel="stylesheet" type="text/css" href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css">
 
-        <!-- Custom (Made by Us)-->
+        <!-- Custom (Made by Us) -->
         <link rel="stylesheet" type="text/css" href="{{ asset('css/patterns.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/master.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/components/text-fields.css') }}">
@@ -41,16 +41,16 @@
             </div>
         @show
 
-        <!-- Sweet Alert -->
+        <!-- SweetAlert2 (By SweetAlert2) -->
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/sweetalert2@7.33.1/dist/sweetalert2.all.min.js"></script>
 
-        <!-- jQuery -->
+        <!-- jQuery (By jQuery) -->
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js"></script>
 
         <!-- jQuery Mask Plugin (By Igor Escobar) -->
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery-mask-plugin@1.14.15/dist/jquery.mask.min.js"></script>
 
-        <!-- Custom (Made by Us)-->
+        <!-- Custom (Made by Us) -->
         <script type="text/javascript" src="{{ asset('js/custom/api.js') }}"></script>
         <script type="text/javascript" src="{{ asset('js/custom/swal.js') }}"></script>
         <script type="text/javascript" src="{{ asset('js/custom/custom-loading.js') }}"></script>
