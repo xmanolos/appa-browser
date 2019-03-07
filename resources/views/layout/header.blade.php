30b4bba88cb4e136d263088d38dbd4227b5ad540
gabrielsecchi <gabrielsecchi@hotmail.com>
2019-03-07 12:54:02 -0300

    improvment show header

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,9 @@
+<div class="header">
+    <div class="panel-logo"></div>
+    <div class="panel-db-data">
+        <label>xmp</label>
+    </div>
+    <div class="panel-exit">
+        <a href="{{ route('api.connection.disconnect') }}" title="Exit"><i class="la la-close"></i></a>
+    </div>
+</div>
