ada45cce28c80a10066872fd156e05787d432095
gabrielsecchi <gabrielsecchi@hotmail.com>
2019-03-18 21:57:36 -0300

    REF #38 - Implemented improvment

--- app/Business/Query/QueryResponse.php
+++ app/Business/Query/QueryResponse.php
@@ -7,8 +7,26 @@ class QueryResponse
     public static function getSuccess($queryType, $message = 'Query executed successfully!', $data = null)
     {
     	$responseArray = ['type' => $queryType, 'state' => 'success', 'message' => $message, 'data' => $data];
-
+        QueryResponse::utf8_encode_deep($responseArray);
     	return \json_encode($responseArray);
+    }
+
+    public static function utf8_encode_deep(&$input) {
+        if (is_string($input)) {
+            $input = utf8_encode($input);
+        } else if (is_array($input)) {
+            foreach ($input as &$value) {
+                QueryResponse::utf8_encode_deep($value);
+            }
+
+            unset($value);
+        } else if (is_object($input)) {
+            $vars = array_keys(get_object_vars($input));
+
+            foreach ($vars as $var) {
+                QueryResponse::utf8_encode_deep($input->$var);
+            }
+        }
     }
 
     public static function getError(\Exception $exception, $message = 'Failed to execute query!')
