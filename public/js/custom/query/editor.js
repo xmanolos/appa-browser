7891c90f7dddf0616cd893c246553b8ea888bdc6
Daniel Freitas <daniel1208x@gmail.com>
2019-05-01 22:29:34 -0300

    Add: query execution based on selected text. (#96)

---
+++ public/js/custom/query/editor.js
@@ -0,0 +1,39 @@
+class QueryEditor {
+    constructor(container) {
+        this.container = container;
+        this.editor = null;
+    }
+
+    initialize() {
+        this.editor = ace.edit(this.container);
+        this.editor.setTheme("ace/theme/sqlserver");
+        this.editor.session.setMode("ace/mode/sql");
+        this.editor.renderer.setOption("showPrintMargin", false);
+    }
+
+    isEmpty() {
+        return (this.getAllText() == null) || (this.getAllText() === "");
+    }
+
+    getQuery() {
+        let selectedText = this.getSelectedText();
+
+        if (selectedText) {
+            return selectedText;
+        }
+
+        return this.getAllText();
+    }
+
+    getSelectedText() {
+        return this.editor.getSelectedText();
+    }
+
+    getAllText() {
+        return this.editor.getValue();
+    }
+
+    setText(text) {
+        this.editor.setValue(text);
+    }
+}
